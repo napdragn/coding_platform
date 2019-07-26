cf2425650b1ca56837507a4b00aa310051b3fbfc
Nagarjun <nagarjun.prasad@go-mmt.com>
2019-07-27 02:49:51 +0530

    Updated get question details api

--- go_hire/contests/api.py
+++ go_hire/contests/api.py
@@ -1,3 +1,4 @@
+import datetime
 import json
 
 from contests.models import Contest, ContestQuestionMapping, UserContest
@@ -71,12 +72,13 @@ class BeginContestApi(APIView):
                 "error_message": "Unable to find any untaken contests."
             }
             return Response(resp_dict, status=status.HTTP_200_OK)
+        user_contest_obj.user_start_time = datetime.datetime.now()
         contest_question_id_list = ContestQuestionMapping.objects.filter(
             contest_id=contest_id
         ).values_list('question_id', flat=True)
         question_details = Question.objects.filter(
             id__in=contest_question_id_list
-        ).values('id', 'type', 'description', 'title')
+        ).values('id', 'type', 'description', 'question_text')
         for q in question_details:
             if q.get('type') == 'mcq':
                 q['options'] = Answer.objects.filter(question_id=q.get('id')).first().expected_input.split(',')
@@ -88,3 +90,52 @@ class BeginContestApi(APIView):
 
     def get(self, request, **kwargs):
         pass
+
+
+# class GetQuestionDetailsApi(APIView):
+#     serializer_class = BeginContestSerializer
+
+#     def post(self, request, **kwargs):
+#         if not request.user.is_authenticated:
+#             resp_dict = {
+#                 "success": False,
+#                 "error_message": "User not authenticated, Please Login."
+#             }
+#             return Response(resp_dict, status=status.HTTP_200_OK)
+#         post_data = json.loads(request.data)
+#         email = request.user.username
+#         contest_id = post_data.get('contest_id', '')
+#         if not contest_id:
+#             resp_dict = {
+#                 "success": False,
+#                 "error_message": "Missing contest_id"
+#             }
+#             return Response(resp_dict, status=status.HTTP_200_OK)
+#         user_contest_obj = UserContest.objects.filter(
+#             contest_id=contest_id,
+#             email=email,
+#             user_start_time__isnull=True
+#         )
+#         if not user_contest_obj:
+#             resp_dict = {
+#                 "success": False,
+#                 "error_message": "Unable to find any untaken contests."
+#             }
+#             return Response(resp_dict, status=status.HTTP_200_OK)
+#         contest_question_id_list = ContestQuestionMapping.objects.filter(
+#             contest_id=contest_id
+#         ).values_list('question_id', flat=True)
+#         question_details = Question.objects.filter(
+#             id__in=contest_question_id_list
+#         ).values('id', 'type', 'description', 'question_text')
+#         for q in question_details:
+#             if q.get('type') == 'mcq':
+#                 q['options'] = Answer.objects.filter(question_id=q.get('id')).first().expected_input.split(',')
+#         resp_dict = {
+#             "success": True,
+#             "questions_list": q
+#         }
+#         return Response(resp_dict, status=status.HTTP_200_OK)
+
+#     def get(self, request, **kwargs):
+#         pass
