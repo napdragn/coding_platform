7cb5f8ad086feeac9f076867fcb12bb0cece3d70
sp1rs <shashank201101193@gmail.com>
2019-07-26 22:02:15 +0530

    Added middleware.

--- go_hire/go_hire/settings.py
+++ go_hire/go_hire/settings.py
@@ -55,6 +55,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'go_hire.middleware.OptionMiddleware',
 ]
 
 ROOT_URLCONF = 'go_hire.urls'
