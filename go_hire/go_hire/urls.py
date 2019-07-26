384e536ed3583bf26041b07047145235ba8885b8
sp1rs <shashank201101193@gmail.com>
2019-07-27 03:55:51 +0530

    Added interview and feedback.

--- go_hire/go_hire/urls.py
+++ go_hire/go_hire/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     url(r'^special/', views.special, name='special'),
     url(r'^user/', include('user.urls')),
     url(r'^logout/$', views.user_logout, name='logout'),
-    url(r'^judge/', include('judge.urls'))
+    url(r'^judge/', include('judge.urls')),
+    url(r'^hiring/', include('interview.urls')),
 ]
 
