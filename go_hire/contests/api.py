de914d08031d31fdf5bcaa03086f783cc6ca695e
Nagarjun <nagarjun.prasad@go-mmt.com>
2019-07-27 03:36:51 +0530

    Added get questions details API

--- go_hire/contests/api.py
+++ go_hire/contests/api.py
@@ -87,7 +87,7 @@ class BeginContestApi(APIView):
                 q['options'] = Answer.objects.filter(question_id=q.get('id')).first().expected_input.split(',')
         resp_dict = {
             "success": True,
-            "questions_list": q
+            "questions_list": question_details
         }
         return Response(resp_dict, status=status.HTTP_200_OK)
 
@@ -95,56 +95,66 @@ class BeginContestApi(APIView):
         pass
 
 
-# class GetQuestionDetailsApi(APIView):
-#     serializer_class = BeginContestSerializer
+class GetQuestionDetailsApi(APIView):
+    serializer_class = BeginContestSerializer
 
-#     def post(self, request, **kwargs):
-#         if not request.user.is_authenticated:
-#             resp_dict = {
-#                 "success": False,
-#                 "error_message": "User not authenticated, Please Login."
-#             }
-#             return Response(resp_dict, status=status.HTTP_200_OK)
-#         post_data = json.loads(request.data)
-#         email = request.user.username
-#         contest_id = post_data.get('contest_id', '')
-#         if not contest_id:
-#             resp_dict = {
-#                 "success": False,
-#                 "error_message": "Missing contest_id"
-#             }
-#             return Response(resp_dict, status=status.HTTP_200_OK)
-#         user_contest_obj = UserContest.objects.filter(
-#             contest_id=contest_id,
-#             email=email,
-#             user_start_time__isnull=True
-#         )
-#         if not user_contest_obj:
-#             resp_dict = {
-#                 "success": False,
-#                 "error_message": "Unable to find any untaken contests."
-#             }
-#             return Response(resp_dict, status=status.HTTP_200_OK)
-#         contest_question_id_list = ContestQuestionMapping.objects.filter(
-#             contest_id=contest_id
-#         ).values_list('question_id', flat=True)
-#         question_details = Question.objects.filter(
-#             id__in=contest_question_id_list
-#         ).values('id', 'type', 'description', 'question_text')
-#         for q in question_details:
-#             if q.get('type') == 'mcq':
-#                 q['options'] = Answer.objects.filter(question_id=q.get('id')).first().expected_input.split(',')
-#         resp_dict = {
-#             "success": True,
-#             "questions_list": q
-#         }
-#         return Response(resp_dict, status=status.HTTP_200_OK)
+    def post(self, request, **kwargs):
+        pass
 
-#     def get(self, request, **kwargs):
-#         pass
+    def get(self, request, **kwargs):
+        get_data = request.GET.dict()
+        email = request.user.username
+        question_id = get_data.get('question_id', '')
+        contest_id = get_data.get('contest_id', '')
+        if not question_id:
+            resp_dict = {
+                "success": False,
+                "error_message": "Missing question_id"
+            }
+            return Response(resp_dict, status=status.HTTP_200_OK)
+        user_contest_obj = UserContest.objects.filter(
+            contest_id=contest_id,
+            email=email,
+            user_end_time__isnull=True
+        )
+        if not user_contest_obj:
+            resp_dict = {
+                "success": False,
+                "error_message": "Not authorized to view this question."
+            }
+            return Response(resp_dict, status=status.HTTP_200_OK)
+        contest_question_id_list = ContestQuestionMapping.objects.filter(
+            contest_id=contest_id
+        ).values_list('question_id', flat=True)
+        if question_id not in contest_question_id_list:
+            resp_dict = {
+                "success": False,
+                "error_message": "Not authorized to view this question."
+            }
+            return Response(resp_dict, status=status.HTTP_200_OK)
+
+        question_obj = Question.objects.filter(
+            id=question_id
+        )
+        if not question_obj:
+            resp_dict = {
+                "success": False,
+                "error_message": "Invalid Question Id given."
+            }
+        else:
+            question_details = question_obj.values('description', 'question_text')
+            resp_dict = {
+                "success": True,
+                "question_details": question_details
+            }
+        return Response(resp_dict, status=status.HTTP_200_OK)
+        
 
 class SubmitContestApi(APIView):
     serializer_class = SubmitContestSerializer
+
+    def get(self, request, **kwargs):
+        pass
 
     def post(self, request, **kwargs):
         if not request.user.is_authenticated:
--- go_hire/contests/urls.py
+++ go_hire/contests/urls.py
@@ -1,4 +1,4 @@
-from contests.api import GetContestsApi, BeginContestApi, SubmitContestApi
+from contests.api import GetContestsApi, BeginContestApi, SubmitContestApi, GetQuestionDetailsApi
 from django.urls import path
 from rest_framework import routers
 
@@ -10,4 +10,5 @@ urlpatterns = [
     path("get_contests", GetContestsApi.as_view()),
     path("begin_contest", BeginContestApi.as_view()),
     path("submit_contest", SubmitContestApi.as_view()),
+    path("get_question_details", GetQuestionDetailsApi.as_view()),
 ]
